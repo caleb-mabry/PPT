c85383d27e20a4ef9c02cc914d2595e4d243d9f6
Caleb Mabry <caleb.mabry.15@cnu.edu>
2020-03-22 18:16:46 -0400

    The actual inital commit

---
+++ chart.js
@@ -0,0 +1,18 @@
+const chartJS = require('chart.js');
+
+const labels = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+const Dataset = {
+    "label": "",
+    "fill": false,
+    "borderColor": "",
+    "data": []
+}
